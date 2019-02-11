0dc49673e3faa81a6eec52b04674ec9727b2fca0
Keith Veleba <keith@veleba.net>
2019-02-11 13:03:15 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+import "./styles.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("3");
+  const [search, setSearch] = useState("");
+
+  const fetchData = async () => {
+    const result = await axios(
+      "http://api.allthingsflexo.com/items?partno=${query}"
+    );
+    setData(result);
+  };
+
+  useEffect(
+    () => {
+      fetchData();
+    },
+    [query]
+  );
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="button" onClick={() => setSearch(query)}>
+          Search
+        </button>
+      </div>
+      <ul>
+        {data.map(i => (
+          <li key={i.id}>
+            <a href={i.imageUrl}>{i.description}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
